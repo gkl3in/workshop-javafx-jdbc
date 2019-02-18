acea5ad31e6369824f2c7c8e96aee7e759ca522c
Gabriel Klein <gabriel.klein@philips.com>
2019-02-18 05:50:24 -0300

    Add files via upload
    Realizada a conexão com o banco de dados.

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -7,8 +7,8 @@ public class Department implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
-	private String  name;
-
+	private String name;
+	
 	public Department() {
 	}
 
@@ -35,8 +35,8 @@ public class Department implements Serializable {
 
 	@Override
 	public int hashCode() {
-		final int prime  = 31;
-		int       result = 1;
+		final int prime = 31;
+		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
